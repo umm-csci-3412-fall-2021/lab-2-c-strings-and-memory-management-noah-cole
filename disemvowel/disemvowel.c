a6689520b39bfb6efe74599ef596cc83073028d7
zeus-ctrl <noah.constable@gmail.com>
2021-09-28 11:20:09 -0500

    Refactor isVowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,39 +1,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <ctype.h>
 #include "disemvowel.h"
 
 bool *isVowel(char test) {
   bool vowel = false;
 
-  switch(test) {
-    case 'A':
-      vowel = true;
-      break;
+  switch(tolower(test)) {
     case 'a':
-      vowel = true;
-      break;
-    case 'E':
-      vowel = true;
-      break;
     case 'e':
-      vowel = true;
-      break;
-    case 'I':
-      vowel = true;
-      break;
     case 'i':
-      vowel = true;
-      break;
-    case 'O':
-      vowel = true;
-      break;
     case 'o':
-      vowel = true;
-      break;
-    case 'U':
-      vowel = true;
-      break;
     case 'u':
       vowel = true;
       break;
