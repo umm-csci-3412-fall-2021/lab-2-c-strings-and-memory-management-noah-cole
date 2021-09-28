86d5fd2847370be11f3037111d2daecd2a22fd6d
zeus-ctrl <noah.constable@gmail.com>
2021-09-28 11:31:43 -0500

    disemvowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
+
 #include "disemvowel.h"
 
 bool *isVowel(char test) {
@@ -22,5 +24,15 @@ bool *isVowel(char test) {
 }
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  char* newString = calloc(strlen(str) + 1, sizeof(str));
+  int placeIndex = 0;
+
+  while (*(str) != "\0") {
+    if(isVowel(str)) {
+      newString[placeIndex++] = str;
+    }
+    str++;
+  }
+  newString[placeIndex] = "\0";
+  return newString;
 }
